cdf47a99042be8ef27cef0e9b4606583b983580e
diogo2299 <100359206+diogo2299@users.noreply.github.com>
2022-06-07 14:59:48 +0100

    Zombie Sound

--- CGS - PROJETO/Assets/Scripts/ZombieApear.cs
+++ CGS - PROJETO/Assets/Scripts/ZombieApear.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class ZombieApear : MonoBehaviour
 {
     public GameObject zombie;
+    public AudioSource ZombieSound;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         zombie.SetActive(true);
+        ZombieSound.Play();
     }
 }
